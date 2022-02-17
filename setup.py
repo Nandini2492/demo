13ca0ffe7b02ca6ddf9f687b5626406c83b9d3ce
Nandini <nandini.angadi@bdb.ai>
2022-02-17 18:01:21 +0530

    changes gj

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='demo',
     version='0.0.3',
     description='Testing installation of Package',
-    long_description=long_description,
+
     long_description_content_type="text/markdown",
     url='https://github.com/Nandini2492/demo.git',
     packages=['demo'],
